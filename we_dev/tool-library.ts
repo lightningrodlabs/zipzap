adaa431cadb09e2ffcabd73513853657ca5d0cb5
Eric Harris-Braun <eric@harris-braun.com>
2024-06-21 15:54:40 -0400

    tool-library update

--- we_dev/tool-library.ts
+++ we_dev/tool-library.ts
@@ -2,8 +2,8 @@ const ToolData =
 {
    icon: "./zipzap_icon.svg",
    name: "ZipZap",
-   subtitle: "Ephemeral Direct Messaging & Chat",
+   subtitle: "Ephemeral direct-messaging & chat",
    version: "0.2.0",
    description: "Quick messaging for all those times that you just want to zip something to someone and then have it be zapped.  Messages are never stored, only in memory.",
-   webhapp: "https://github.com/holochain-apps/kando/releases/download/v0.10.2/kando.webhapp",
+   webhapp: "https://github.com/lightningrodlabs/zipzap/releases/download/v0.2.0/zipzap.webhapp",
 }
